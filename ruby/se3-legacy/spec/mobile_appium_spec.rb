9b8f6b7bbbec5667e2e1cb99a48992b35464a44e
titusfortner <titusfortner@gmail.com>
2021-08-31 18:49:45 -0500

    [ruby] update mobile capability order

--- ruby/se3-legacy/spec/mobile_appium_spec.rb
+++ ruby/se3-legacy/spec/mobile_appium_spec.rb
@@ -9,85 +9,84 @@ describe 'Mobile with Appium' do
 
   describe 'Browser' do
     it 'runs latest Android with latest Appium' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.20.2'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "11.0"
       caps['platformName'] = 'Android'
       caps['browserName'] = 'Chrome'
+      caps['appium:deviceName'] = 'Android GoogleAPI Emulator'
+      caps['appium:platformVersion'] = '11.0'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.20.2'
 
       start_appium_driver(caps)
       validate_google
     end
 
     it 'runs latest Android with earliest Appium' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.15.0'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "11.0"
       caps['platformName'] = 'Android'
       caps['browserName'] = 'Chrome'
+      caps['appium:deviceName'] = 'Android GoogleAPI Emulator'
+      caps['appium:platformVersion'] = '11.0'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.15.0'
 
       start_appium_driver(caps)
       validate_google
     end
 
     it 'runs earliest Android with latest Appium' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.20.2'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "5.1"
       caps['platformName'] = 'Android'
       caps['browserName'] = 'Browser'
+      caps['appium:deviceName'] = 'Android Emulator'
+      caps['appium:platformVersion'] = '5.1'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.20.2'
 
       start_appium_driver(caps)
       validate_google
     end
 
     it 'runs earliest Android with earliest Appium Stated' do
-      skip('1.8.0 does not convert timeout calls, so get error')
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.8.0'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "5.1"
       caps['platformName'] = 'Android'
       caps['browserName'] = 'Browser'
+      caps['appium:deviceName'] = 'Android Emulator'
+      caps['appium:platformVersion'] = '5.1'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.8.0'
 
       start_appium_driver(caps)
       validate_google
     end
 
     it 'runs earliest Android with earliest Appium Actual' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.9.1'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "6.0"
       caps['platformName'] = 'Android'
       caps['browserName'] = 'Browser'
+      caps['appium:deviceName'] = 'Android Emulator'
+      caps['appium:platformVersion'] = '5.1'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.9.1'
 
       start_appium_driver(caps)
       validate_google
     end
 
     it 'runs latest iOS' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.21.0'
-      caps['appium:deviceName'] = "iPhone Simulator"
-      caps['appium:platformVersion'] = "14.5"
       caps['platformName'] = 'iOS'
       caps['browserName'] = 'Safari'
+      caps['appium:deviceName'] = 'iPhone Simulator'
+      caps['appium:platformVersion'] = '14.5'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.20.1'
 
       start_appium_driver(caps)
       validate_google
     end
 
     it 'with earliest iOS' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.8.0'
-      caps['appium:deviceName'] = "iPhone Simulator"
-      caps['appium:platformVersion'] = "10.3"
       caps['platformName'] = 'iOS'
       caps['browserName'] = 'Safari'
+      caps['appium:deviceName'] = 'iPhone 6s Simulator'
+      caps['appium:platformVersion'] = '10.3'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.8.0'
 
       start_appium_driver(caps)
       validate_google
@@ -96,13 +95,12 @@ describe 'Mobile with Appium' do
 
   describe 'Native App' do
     it 'runs latest Android with latest Appium' do
+      caps['platformName'] = 'Android'
+      caps['appium:app'] = android
+      caps['appium:deviceName'] = 'Android GoogleAPI Emulator'
+      caps['appium:platformVersion'] = '11.0'
       caps['sauce:options'] = {}
       caps['sauce:options']['appiumVersion'] = '1.20.2'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "11.0"
-      caps['platformName'] = 'Android'
-      caps['browserName'] = ''
-      caps['appium:app'] = android
       caps['appium:appWaitActivity'] = 'com.swaglabsmobileapp.MainActivity'
 
       start_appium_driver(caps)
@@ -110,13 +108,12 @@ describe 'Mobile with Appium' do
     end
 
     it 'runs latest Android with earliest Appium' do
+      caps['platformName'] = 'Android'
+      caps['appium:app'] = android
+      caps['appium:deviceName'] = 'Android GoogleAPI Emulator'
+      caps['appium:platformVersion'] = '11.0'
       caps['sauce:options'] = {}
       caps['sauce:options']['appiumVersion'] = '1.15.0'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "11.0"
-      caps['platformName'] = 'Android'
-      caps['browserName'] = ''
-      caps['appium:app'] = android
       caps['appium:appWaitActivity'] = 'com.swaglabsmobileapp.MainActivity'
 
       start_appium_driver(caps)
@@ -124,13 +121,12 @@ describe 'Mobile with Appium' do
     end
 
     it 'runs earliest Android with latest Appium' do
+      caps['platformName'] = 'Android'
+      caps['appium:app'] = android
+      caps['appium:deviceName'] = 'Android GoogleAPI Emulator'
+      caps['appium:platformVersion'] = '5.1'
       caps['sauce:options'] = {}
       caps['sauce:options']['appiumVersion'] = '1.20.2'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "5.1"
-      caps['platformName'] = 'Android'
-      caps['browserName'] = ''
-      caps['appium:app'] = android
       caps['appium:appWaitActivity'] = 'com.swaglabsmobileapp.MainActivity'
 
       start_appium_driver(caps)
@@ -138,13 +134,12 @@ describe 'Mobile with Appium' do
     end
 
     it 'runs earliest Android with earliest Appium' do
+      caps['platformName'] = 'Android'
+      caps['appium:app'] = android
+      caps['appium:deviceName'] = 'Android GoogleAPI Emulator'
+      caps['appium:platformVersion'] = '5.1'
       caps['sauce:options'] = {}
       caps['sauce:options']['appiumVersion'] = '1.8.0'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "5.1"
-      caps['platformName'] = 'Android'
-      caps['browserName'] = ''
-      caps['appium:app'] = android
       caps['appium:appWaitActivity'] = 'com.swaglabsmobileapp.MainActivity'
 
       start_appium_driver(caps)
@@ -152,13 +147,12 @@ describe 'Mobile with Appium' do
     end
 
     it 'runs latest iOS' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.21.0'
-      caps['appium:deviceName'] = "iPhone Simulator"
-      caps['appium:platformVersion'] = "14.5"
       caps['platformName'] = 'iOS'
-      caps['browserName'] = ''
       caps['appium:app'] = iOS
+      caps['appium:deviceName'] = 'iPhone Simulator'
+      caps['appium:platformVersion'] = '14.5'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.20.1'
 
       start_appium_driver(caps)
       validate_app
@@ -166,26 +160,24 @@ describe 'Mobile with Appium' do
 
     it 'with earliest iOS Stated' do
       skip('10.3 does not want to start an app; skip because takes too long to fail')
+      caps['platformName'] = 'iOS'
+      caps['appium:app'] = iOS
+      caps['appium:deviceName'] = 'iPhone Simulator'
+      caps['appium:platformVersion'] = '10.3'
       caps['sauce:options'] = {}
       caps['sauce:options']['appiumVersion'] = '1.8.0'
-      caps['appium:deviceName'] = "iPhone Simulator"
-      caps['appium:platformVersion'] = "10.3"
-      caps['platformName'] = 'iOS'
-      caps['browserName'] = ''
-      caps['appium:app'] = iOS
 
       start_appium_driver(caps)
       validate_app
     end
 
     it 'with earliest iOS Actual' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.8.0'
-      caps['appium:deviceName'] = "iPhone Simulator"
-      caps['appium:platformVersion'] = "11.0"
       caps['platformName'] = 'iOS'
-      caps['browserName'] = ''
       caps['appium:app'] = iOS
+      caps['appium:deviceName'] = 'iPhone Simulator'
+      caps['appium:platformVersion'] = '10.3'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.9.1'
 
       start_appium_driver(caps)
       validate_app
--- ruby/se4-w3c/spec/mobile_se_spec.rb
+++ ruby/se4-w3c/spec/mobile_se_spec.rb
@@ -7,74 +7,75 @@ describe 'Mobile with Selenium' do
 
   describe 'Browser' do
     it 'runs latest Android with latest Appium' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.20.2'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "11.0"
       caps['platformName'] = 'Android'
       caps['browserName'] = 'Chrome'
+      caps['appium:deviceName'] = 'Android GoogleAPI Emulator'
+      caps['appium:platformVersion'] = '11.0'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.20.2'
 
-      start_mobile_driver(caps)
+      start_driver(caps)
       validate_google
     end
 
     it 'runs latest Android with earliest Appium' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.15.0'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "11.0"
       caps['platformName'] = 'Android'
       caps['browserName'] = 'Chrome'
+      caps['appium:deviceName'] = 'Android GoogleAPI Emulator'
+      caps['appium:platformVersion'] = '11.0'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.15.0'
 
-      start_mobile_driver(caps)
+      start_driver(caps)
       validate_google
     end
 
     it 'runs earliest Android with latest Appium' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.20.2'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "5.1"
       caps['platformName'] = 'Android'
       caps['browserName'] = 'Browser'
+      caps['appium:deviceName'] = 'Android Emulator'
+      caps['appium:platformVersion'] = '5.1'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.20.2'
 
-      start_mobile_driver(caps)
+      start_driver(caps)
       validate_google
     end
 
+    # Conversion issue with timeouts not being recognized as a double
     it 'runs earliest Android with earliest Appium' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.8.0'
-      caps['appium:deviceName'] = "Android GoogleAPI Emulator"
-      caps['appium:platformVersion'] = "5.1"
       caps['platformName'] = 'Android'
       caps['browserName'] = 'Browser'
+      caps['appium:deviceName'] = 'Android Emulator'
+      caps['appium:platformVersion'] = '5.1'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.8.0'
 
-      start_mobile_driver(caps)
+      start_driver(caps)
       validate_google
     end
 
     it 'runs latest iOS' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.21.0'
-      caps['appium:deviceName'] = "iPhone Simulator"
-      caps['appium:platformVersion'] = "14.5"
       caps['platformName'] = 'iOS'
       caps['browserName'] = 'Safari'
+      caps['appium:deviceName'] = 'iPhone Simulator'
+      caps['appium:platformVersion'] = '14.5'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.20.1'
 
-      start_mobile_driver(caps)
+      start_driver(caps)
       validate_google
     end
 
     it 'with earliest iOS' do
-      caps['sauce:options'] = {}
-      caps['sauce:options']['appiumVersion'] = '1.8.0'
-      caps['appium:deviceName'] = "iPhone Simulator"
-      caps['appium:platformVersion'] = "10.3"
       caps['platformName'] = 'iOS'
       caps['browserName'] = 'Safari'
+      caps['appium:deviceName'] = 'iPhone 6s Simulator'
+      caps['appium:platformVersion'] = '10.3'
+      caps['sauce:options'] = {}
+      caps['sauce:options']['appiumVersion'] = '1.8.0'
 
-      start_mobile_driver(caps)
+      start_driver(caps)
       validate_google
     end
   end
